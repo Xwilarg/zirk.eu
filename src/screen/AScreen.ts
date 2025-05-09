6a21061630afe6fc3af02b4d90bf3521f1fb16dd
Christian Chaux <xwilarg@protonmail.com>
2025-05-09 12:29:19 +0000

    Split screen into sub-classes

---
+++ src/screen/AScreen.ts
@@ -0,0 +1,42 @@
+export abstract class AScreen
+{
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+
+    mouseX: number | null = null;
+    mouseY: number | null = null;
+
+    constructor() {
+        this.mouseX = null;
+        this.mouseY = null;
+
+        this.canvas = document.getElementById("screen-canvas") as HTMLCanvasElement;
+        const targetWidth = document.getElementById("screen-container")!.clientWidth;
+        this.canvas.width = targetWidth;
+        this.canvas.height = 450;
+
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Mouse_controls
+    handleMouse(e: MouseEvent) {
+        const bounds = this.canvas.getBoundingClientRect();
+        const relX = e.clientX - bounds.left;
+        const relY = e.clientY - bounds.top;
+        if (relX >= 0 && relX < this.canvas.width && relY >= 0 && relY < this.canvas.height) {
+            this.setMousePos(relX, relY);
+        } else {
+            this.mouseX = null;
+            this.mouseY = null;
+        }
+    }
+    
+    updateCanvas() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.render();
+    }
+
+    abstract render(): void;
+    abstract setMousePos(x: number, y: number): void;
+}
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function randInt(max: number): number {
+    return Math.floor(Math.random() * max);
+}
