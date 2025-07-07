0909c97aa1943b7e9ec895f3e45244aa1e31c624
Xwilarg <xwilarg@protonmail.com>
2025-07-07 10:54:36 +0200

    Improve CSS

--- frontend/scripts/resize.sh
+++ frontend/scripts/resize.sh
@@ -5,24 +5,24 @@ set -e
 echo "Gamejam images"
 mkdir -p ../img/gamejam
 cd ../data/img/gamejam
-magick mogrify -resize "500x400>" -path "../../../img/gamejam/" *.png;
-magick mogrify -resize "500x400>" -path "../../../img/gamejam/" *.jpg;
+magick mogrify -resize "500x400^" -path "../../../img/gamejam/" *.png;
+magick mogrify -resize "500x400^" -path "../../../img/gamejam/" *.jpg;
 cd -
 
 echo "Home images"
 mkdir -p ../img/home
 cd ../data/img/home
-magick mogrify -resize "445x250>" -path "../../../img/home/" *.png;
+magick mogrify -resize "445x250^" -path "../../../img/home/" *.png;
 cd -
 
 echo "Projects images"
 mkdir -p ../img/projects
 cd ../data/img/projects
-magick mogrify -resize "500x400>" -path "../../../img/projects/" *.png;
+magick mogrify -resize "500x400^" -path "../../../img/projects/" *.png;
 cd -
 
 echo "Projects (old) images"
 mkdir -p ../img/projects-old
 cd ../data/img/projects-old
-magick mogrify -resize "500x400>" -path "../../../img/projects-old/" *.png;
+magick mogrify -resize "500x400^" -path "../../../img/projects-old/" *.png;
 cd -
