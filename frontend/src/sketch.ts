0a69d5501641a3235780a72238b48880c11db8e2
Christian Chaux <xwilarg@protonmail.com>
2025-07-25 11:48:33 +0000

    Add movement buttons to sketch

--- frontend/src/sketch.ts
+++ frontend/src/sketch.ts
@@ -1,5 +1,5 @@
-let isSketchLoaded = false;
 let sketchInstance: any;
+let isSketchLoaded = false;
 
 function explanation_show_sketch(id: string) {
     return;
@@ -9,10 +9,12 @@ function explanation_show_sketch(id: string) {
 }
 
 function setupSketchButton(data: HTMLElement) {
-    document.getElementById(data.id)!.addEventListener("click", _ => {
-        sketchInstance.SendMessage('LevelLoader', 'LoadScene', data.dataset.scene);
-        explanation_show_sketch(data.id);
-    });
+    if (data.dataset.scene) {
+        document.getElementById(data.id)!.addEventListener("click", _ => {
+            sketchInstance.SendMessage('LevelLoader', 'LoadScene', data.dataset.scene);
+            explanation_show_sketch(data.id);
+        });
+    }
 }
 
 export function setupSketch() {
@@ -24,7 +26,55 @@ export function setupSketch() {
         document.getElementById("screen-off")!.classList.add("is-hidden");
         document.getElementById("screen-on")!.classList.remove("is-hidden");
         loadSketch();
-    })
+    });
+
+    document.getElementById("fullscreen")!.addEventListener("click", _ => {
+        document.getElementById("fullscreen")!.classList.add("is-hidden");
+        document.getElementById("card-sketch")!.classList.add("fullscreen-view");
+        document.querySelector("#target-main-container .break").classList.remove("is-hidden");
+
+        resizeUnityCanvas();
+    });
+    document.getElementById("keep")!.addEventListener("click", _ => {
+        document.getElementById("keep")!.classList.add("is-hidden");
+
+        let outerDiv = document.getElementById("target-main-container");
+        let innerDiv = document.getElementById("card-sketch");
+        let targetDiv = document.getElementById("pinned-sketch");
+
+        targetDiv.appendChild(outerDiv.removeChild(innerDiv));
+
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+}
+
+function unityShowBanner(msg: string, type: string) {
+    const warningBanner = document.querySelector("#unity-warning") as HTMLElement;
+    function updateBannerVisibility() {
+        warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+    }
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    warningBanner.appendChild(div);
+    if (type == 'error') div.setAttribute("style", "background: red; padding: 10px;");
+    else {
+        if (type == 'warning') div.setAttribute("style", "background: yellow; padding: 10px;");
+        setTimeout(function () {
+            warningBanner.removeChild(div);
+            updateBannerVisibility();
+        }, 5000);
+    }
+    updateBannerVisibility();
+}
+
+function resizeUnityCanvas()
+{
+    const canvas = document.querySelector("#unity-canvas") as HTMLCanvasElement;
+    const container = document.getElementById("unity-container")!;
+    canvas.width = container.clientWidth;
+    canvas.height = container.clientHeight;
+    canvas.style.width = `${container.clientWidth}px`;
+    canvas.style.height = `${container.clientHeight}px`;
 }
 
 function loadSketch() {
@@ -33,25 +83,6 @@ function loadSketch() {
     isSketchLoaded = true;
 
     const canvas = document.querySelector("#unity-canvas") as HTMLCanvasElement;
-    const warningBanner = document.querySelector("#unity-warning") as HTMLElement;
-
-    function unityShowBanner(msg: string, type: string) {
-        function updateBannerVisibility() {
-            warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
-        }
-        var div = document.createElement('div');
-        div.innerHTML = msg;
-        warningBanner.appendChild(div);
-        if (type == 'error') div.setAttribute("style", "background: red; padding: 10px;");
-        else {
-            if (type == 'warning') div.setAttribute("style", "background: yellow; padding: 10px;");
-            setTimeout(function () {
-                warningBanner.removeChild(div);
-                updateBannerVisibility();
-            }, 5000);
-        }
-        updateBannerVisibility();
-    }
 
     var buildUrl = "sketch";
     var loaderUrl = buildUrl + "/Sketch.loader.js";
@@ -66,11 +97,7 @@ function loadSketch() {
         showBanner: unityShowBanner,
     };
 
-    const container = document.getElementById("unity-container")!;
-    canvas.width = container.clientWidth;
-    canvas.height = container.clientHeight;
-    canvas.style.width = `${container.clientWidth}px`;
-    canvas.style.height = `${container.clientHeight}px`;
+    resizeUnityCanvas();
 
     const script = document.createElement("script");
     script.src = loaderUrl;
