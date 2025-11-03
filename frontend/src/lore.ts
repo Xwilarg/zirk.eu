658f7fddbcc3ca90c616cebc8cd200d77731c415
Christian Chaux <xwilarg@protonmail.com>
2025-11-03 09:52:35 +0000

    Update lore page

--- frontend/src/lore.ts
+++ frontend/src/lore.ts
@@ -1,24 +1,8 @@
-import { DataSet, Edge, Network, Node } from "vis-network/standalone";
-import { Timeline } from "vis-timeline/standalone";
-
 let currSummary = null;
 let currDescription = null;
 let showDetails = false;
 
-function clearAll() {
-    document.getElementById("answer-lore-rhefir")!.classList.add("is-hidden");
-    document.getElementById("answer-lore-nehneh")!.classList.add("is-hidden");
-}
-
 export function setupLore() {
-    document.getElementById("lore-rhefir")!.addEventListener("click", _ => {
-        clearAll();
-        document.getElementById("answer-lore-rhefir")!.classList.remove("is-hidden");
-    });
-    document.getElementById("lore-nehneh")!.addEventListener("click", _ => {
-        clearAll();
-        document.getElementById("answer-lore-nehneh")!.classList.remove("is-hidden");
-    });
     for (let btn of document.querySelectorAll(".btn-lore"))
     {
         btn.addEventListener("click", _ => {
@@ -44,87 +28,5 @@ export function setupLore() {
             e.classList.add("is-flex");
         }
         document.getElementById("lore-see-all").classList.add("is-hidden");
-    });
-
-    renderNetwork();
-    renderTimeline();
-}
-
-function renderTimeline() {
-    let json = JSON.parse(document.getElementById("lore-timeline-json")!.innerHTML);
-
-    const container = document.getElementById('lore-timeline') as HTMLElement;
-
-    const options = {
-        stack: false
-    };
-
-    new Timeline(container, new DataSet(json.data), new DataSet(json.groups), options);
-}
-
-interface NodeData extends Node
-{
-    id: string
-    label: string
-    group: string
-    color: string
-    description: string
-    size: number
-}
-
-let links: Edge[] = [];
-let nodes: NodeData[] = [];
-
-let colors = [ "#bfbfbf", "#7fff94ff", "#7f8cffff", "#9714d9ff", "#d91414ff", "#7fc8ffff" ];
-let categories = [ "origin", "transfert", "dragonborn", "hominidae", "energy", "chimera" ];
-
-function renderNetwork() {
-    let json = JSON.parse(document.getElementById("lore-species-json")!.innerHTML);
-
-    const container = document.getElementById('lore-species-network') as HTMLElement;
-
-    for (const l of json.data) {
-        let group = categories.indexOf(l.category);
-        nodes.push({
-            id: l.id,
-            label: l.name,
-            group: l.category,
-            color: colors[group],
-            description: l.description.join("\n"),
-            size: l.type === "abstract" ? 2 : 25
-        });
-        for (const child of l.children)
-        {
-            links.push({ from: l.id, to: child });
-        }
-    }
-
-    const data = {
-        nodes: new DataSet<Node>(nodes),
-        edges: new DataSet<Edge>(links)
-    };
-    const options = {
-        nodes: {
-            shape: "dot",
-            font: {
-                color: "white",
-                size: 18
-            }
-        },
-        interaction: {
-            zoomView: false
-        },
-        width: "600px"
-    };
-
-    const network = new Network(container, data, options);
-    network.on('click', function(properties) {
-        var ids = properties.nodes;
-        if (ids.length > 0) {
-            (document.getElementById("lore-species-text") as HTMLTextAreaElement).value = nodes.find(x => x.id === ids[0]).description;
-        }
-    });
-    network.on("stabilizationIterationsDone", function () {
-        network.setOptions( { physics: false } );
     });
 }
