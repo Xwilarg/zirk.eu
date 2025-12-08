6089a9a0b86c5801feef53a65dd02b6c978fa794
Xwilarg <xwilarg@protonmail.com>
2025-12-08 20:44:28 +0100

    Add screen buttons

--- src/computer/ComputerForm.tsx
+++ src/computer/ComputerForm.tsx
@@ -1,7 +1,13 @@
 import ScreenSaverForm from "./impl/ScreenSaverForm";
 
 export default function SketchForm() {
-    return <div className="container">
-        <ScreenSaverForm/>
-    </div>
+    return <>
+        <div className="container">
+            <ScreenSaverForm/>
+        </div>
+        <div className="container is-flex">
+            <button><span className="material-symbols-outlined">power_settings_new</span></button>
+            <button><span className="material-symbols-outlined">eject</span></button>
+        </div>
+    </>
 }
--- src/computer/impl/ScreenSaverForm.tsx
+++ src/computer/impl/ScreenSaverForm.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef } from "react";
 import { AScreen } from "./screensaver/AScreen";
 import { BallsScreen } from "./screensaver/BallsScreen";
+import { ParticleOrbits } from "./screensaver/ParticleOrbit";
+import { randInt } from "../../utils";
 
 export default function ScreenSaverForm() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const screenSaverRef = useRef<AScreen | null>(null);
     
     useEffect(() => {
-        const sc = new BallsScreen(canvasRef.current!);
+        const elems = [ParticleOrbits, BallsScreen];
+        const sc = new elems[randInt(elems.length)](canvasRef.current!);
         screenSaverRef.current = sc;
 
         document.addEventListener("mousemove", sc.handleMouse.bind(sc));
@@ -21,8 +24,5 @@ export default function ScreenSaverForm() {
         window.requestAnimationFrame(updateLoop);
     }
 
-    return <canvas ref={canvasRef} style={{
-        height: "500px",
-        width: "100%"
-    }}></canvas>
+    return <canvas ref={canvasRef} id="screensaver-canvas"></canvas>
 }
