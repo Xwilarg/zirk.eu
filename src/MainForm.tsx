fe0d7ccaaf817ac1e0b915930f923368089eb6e7
Xwilarg <xwilarg@protonmail.com>
2025-12-10 19:10:50 +0100

    Improve intro

--- src/MainForm.tsx
+++ src/MainForm.tsx
@@ -1,14 +1,45 @@
+import { Link } from "react-router"
 import "../css/index.css"
 import SketchForm from "./computer/ComputerForm"
 import NavigationForm from "./NavigationForm"
+import { useState } from "react";
+import sheepData from "../data/json/sheep.json"
+
+interface SheepInfo
+{
+    image: string
+}
 
 export default function MainForm() {
+    let [showSheep, setShowSheep] = useState<boolean>(false);
+    let [sheep, setSheep] = useState<SheepInfo[]>(sheepData);
+
     return <>
         <NavigationForm />
-        <div className="container">
-            Welcome on my website, I wanted to finish this before going to sleep so I decided to push this website with most features missing<br/>
-            My hobbies are putting dotted underline decoration on my &lt;a&gt; tags and lying about my hobbies<br/>
-            On the meantime to have more features you can send me a mail to <a href="mailto:xwilarg@protonmail.com">xwilarg@protonmail.com</a> and tell me all about your favorite cheese
+        <div className={showSheep ? "container enlarged" : "container"} id="intro">
+            Welcome on my website, I am Zirk, a game and software developer<br/>
+            <br/>
+            I am probably mostly known for <span className="katsis-highlight">Katsis</span> (which I co-created with Fractal) and <Link to="/gamejam">participating at gamejams</Link><br/>
+            I like lot of others little hobbies that I might hide around on this website at a future date<br/>
+            <br/>
+            And speaking of this website, ta-da here you are, it's still a big work in progress but hopefully it should come closer to an aesthetic I like<br/>
+            On the meantime, I hope you enjoy your stay here :)<br/>
+            <br/>
+            If you scrolled up here, why not contributing to my <a onClick={_ => setShowSheep(x => !x)}>sheep collection</a>?
+            <br/>
+            {
+                showSheep ?
+                <div className="is-flex">
+                    {
+                        sheep.map(x => 
+                            <div className="sheep-img">
+                                <img src={`/data/img/sheep/${x.image}`} />
+                            </div>
+                        )
+                    }
+                </div>
+                : <></>
+            }
         </div>
         <SketchForm />
     </> 
