e7571a0bd0015e6f89d1d82bc5d5eecf547408f1
Xwilarg <xwilarg@protonmail.com>
2026-01-25 17:02:43 +0100

    Move quotes to data

--- src/components/QuoteComponent.tsx
+++ src/components/QuoteComponent.tsx
@@ -1,20 +1,7 @@
 import { randInt } from "../utils";
+import quotesData from "../../data/json/quotes.json"
 
-const quotes = [
-    "Now with 20% more minipis!",
-    "Still awaiting Indra new website",
-    "Fighting to get a better UX than Fractal since 2023 (I'm not winning)",
-    "Red and green gives yellow",
-    "<a href='/secret/quote' class='ignore'>Of course you can't click this quote!</a>",
-    //"[object Object]",
-    "My favorite ice creams are pistachio and rum raisin",
-    "Eöna, Sïon, Noïra, Laïka, Mïa, Sër, Misïe, Fäe, Füre",
-    "If your sentence start by \"Imagine\" you already lost",
-    "<a href='https://youtu.be/OtQZ_eyVhKQ target='_blank' class='ignore-half'>Click here for my favorite YouTube video</a>",
-    "Welcome to the world of boxes",
-    "My favorite C# operator is the null-coalescing one",
-    "Why is demisexual flag colors so dim"
-];
+const quotes = quotesData;
 
 export default function QuoteComponent() {
     let randomQuote = randInt(quotes.length);
