1320ed926424cb9cfe988f5bec437134ec383763
Xwilarg <xwilarg@protonmail.com>
2026-02-18 18:00:08 +0100

    Update prepare script

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 New-Item -ItemType Directory -Force -Path ../public/data/img/
+New-Item -ItemType Directory -Force -Path ../public/data/old/projects-old/
 New-Item -ItemType Directory -Force -Path ../public/gamejam/
 Copy-Item -Path ../data/img/about/ -Destination ../public/data/img/ -Recurse -Force
 Copy-Item -Path ../data/img/boxes/ -Destination ../public/data/img/ -Recurse -Force
@@ -9,3 +10,4 @@ Copy-Item -Path ../data/img/sheep/ -Destination ../public/data/img/ -Recurse -Fo
 Copy-Item -Path ../data/img/steam/ -Destination ../public/data/img/ -Recurse -Force
 Copy-Item -Path ../data/img/website/ -Destination ../public/data/img/ -Recurse -Force
 Copy-Item -Path ../gamejam -Destination ../public/ -Recurse -Force
+Copy-Item -Path ../data/old/projects-old/ -Destination ../public/data/old/projects-old/ -Recurse -Force
