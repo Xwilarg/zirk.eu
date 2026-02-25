f4df7a23efe2f1f3fe77a4d9182271becc6c5b9c
Xwilarg <xwilarg@protonmail.com>
2026-02-25 23:51:19 +0100

    Add quote icons

--- src/components/QuoteComponent.tsx
+++ src/components/QuoteComponent.tsx
@@ -1,10 +1,10 @@
-import { randInt } from "../utils";
+import { isNsfw, randInt } from "../utils";
 import quotesData from "../../data/json/quotes.json"
 
 const quotes = quotesData;
 
 export default function QuoteComponent() {
-    const flattenQuotes = quotes.map(x => x.data).reduce((a, b) => a.concat(b));
+    const flattenQuotes = quotes.filter(x => isNsfw() === "NSFW" || x.id !== "nsfw").map(x => x.data).reduce((a, b) => a.concat(b));
     let randomQuote = randInt(flattenQuotes.length);
 
     return <div id="intro-top">
