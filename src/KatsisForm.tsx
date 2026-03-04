9faede3715bcf074e60341b0bf015ffe4be7c3ed
Xwilarg <xwilarg@protonmail.com>
2026-03-04 23:08:08 +0100

    Improve Katsis page

--- src/KatsisForm.tsx
+++ src/KatsisForm.tsx
@@ -1,11 +1,78 @@
 import QuoteComponent from "./components/QuoteComponent";
 import NavigationComponent from "./components/NavigationComponent";
 import KatsisIntroComponent from "./components/intro/KatsisIntroComponent";
+import { useEffect, useState, type ReactElement } from "react";
+import { isNsfw } from "./utils";
+
+interface KatsisProject
+{
+    games: Array<KatsisProjectProject>
+}
+
+interface KatsisProjectProject
+{
+    name: string
+    thumbnail: string
+}
 
 export default function KatsisForm() {
+    const [katsisProject, setKatsisProject] = useState<KatsisProject | null>(null);
+    const [katsisHtml, setKatsisHtml] = useState<ReactElement[]>([]);
+
+    let nsfwStatus = isNsfw();
+
+    useEffect(() => {
+        fetch('https://intranet.katsis.net/api/user/public/1')
+        .then(resp => { if (resp.ok) return resp.json(); throw new Error(""); })
+        .then(json => {
+            json.games.reverse();
+            setKatsisProject(json);
+        })
+        .catch(_ => {});
+    }, [ ]);
+
+    useEffect(() => {
+        let data: ReactElement[] = [];
+
+        if (katsisProject)
+        {
+            for (let p of katsisProject!.games)
+            {
+                if (nsfwStatus === "NSFW")
+                {
+                    data.push(<div className="card" key={p.name}>
+                        <h2>{p.name}</h2>
+                        <a target="_blank" href={`https://katsis.net/g/${p.thumbnail.split('/')[0]}`}>
+                            <div className="project-img">
+                                <img src={`https://cdn.katsis.net/${p.thumbnail}`} />
+                            </div>
+                        </a>
+                    </div>);
+                }
+                else
+                {
+                    data.push(<div className="card" key={p.name}>
+                        <h2>{p.name}</h2>
+                        <div className="project-img">
+                            <img className="blur" src={`https://cdn.katsis.net/${p.thumbnail}`} />
+                        </div>
+                    </div>);
+                }
+            }
+        }
+
+        setKatsisHtml(data);
+    }, [ katsisProject ]);
+
     return <>
         <QuoteComponent/>
         <KatsisIntroComponent />
         <NavigationComponent />
+        <div className="container box">
+            <p className="mark">Katsis</p>
+            <div className="is-flex flex-center-hor">
+                { katsisHtml }
+            </div>
+        </div>
     </>
 }
--- src/NavigationForm.tsx
+++ src/NavigationForm.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router";
-import { getNavigation } from "./utils";
+import { getNavigation, isNsfw } from "./utils";
 
 export default function NavigationForm() {
+    const nsfwStatus = isNsfw();
+
     return <nav className="is-flex">
         <Link className="nav-button" to={getNavigation("/")}>Home</Link>
         <Link className="nav-button" to={getNavigation("/gamejam")}>Gamejam</Link>
         <Link className="nav-button" to={getNavigation("/project")}>Project</Link>
-        <Link className="nav-button" to={getNavigation("/katsis")}>Katsis</Link>
+        { nsfwStatus == "FullSFW" ? <></> : <Link className="nav-button" to={getNavigation("/katsis")}>Katsis</Link> }
         <Link className="nav-button" to={getNavigation("/life")}>Lifeline</Link>
         <Link className="nav-button" to={getNavigation("/box")}>Boxes</Link>
         <Link className="nav-button" to={getNavigation("/info")}>Info</Link>
