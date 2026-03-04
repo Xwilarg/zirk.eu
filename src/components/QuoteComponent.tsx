eecf021973f4180fe7303ebfe722a0d80b23a6bf
Xwilarg <xwilarg@protonmail.com>
2026-03-04 19:02:57 +0100

    Fix quote always refreshing

--- src/components/QuoteComponent.tsx
+++ src/components/QuoteComponent.tsx
@@ -1,13 +1,15 @@
 import { isNsfw, randInt } from "../utils";
 import quotesData from "../../data/json/quotes.json"
+import { useState } from "react";
 
 const quotes = quotesData;
 
 export default function QuoteComponent() {
     const flattenQuotes = quotes.filter(x => isNsfw() === "NSFW" || x.id !== "nsfw").map(x => x.data).reduce((a, b) => a.concat(b));
-    let randomQuote = randInt(flattenQuotes.length);
+
+    const [quoteIndex, setQuoteIndex] = useState(randInt(flattenQuotes.length));
 
     return <div id="intro-top">
-        <p id="intro-quote" dangerouslySetInnerHTML={{ __html: flattenQuotes[randomQuote] }}></p>
+        <p id="intro-quote" dangerouslySetInnerHTML={{ __html: flattenQuotes[quoteIndex] }}></p>
     </div>
 }
