f99b67e266714ff0c8f8fb563cc4d7ffa7404977
Xwilarg <xwilarg@protonmail.com>
2026-04-05 22:48:16 +0200

    Add Katsis link

--- src/components/intro/KatsisIntroComponent.tsx
+++ src/components/intro/KatsisIntroComponent.tsx
@@ -1,9 +1,13 @@
-export default function KatsisIntroComponent() {
+import { isNsfw } from "../../utils"
+
+export default function KatsisIntroComponent()
+{
+    var nsfwStatus = isNsfw();
 
     return <div className="container box">
         <p className="mark">Introduction</p>
         <div id="intro">
-            Project I participated in as part of <span className="katsis-highlight">Katsis</span>
+            Project {nsfwStatus === "NSFW" ? <a href="https://zirk.katsis.net" target="_blank">I</a> : "I"} participated in as part of <span className="katsis-highlight">Katsis</span>
         </div>
     </div>
 }
