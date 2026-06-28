3ebdcecdfeb0c8edcaf413e48e6c71694eac8364
Xwilarg <xwilarg@protonmail.com>
2026-06-28 12:06:07 +0200

    Embed character sheet

--- src/TtrpgForm.tsx
+++ src/TtrpgForm.tsx
@@ -2,42 +2,9 @@ import NavigationComponent from "./components/NavigationComponent";
 import QuoteComponent from "./components/QuoteComponent";
 import { useEffect, useState, type ReactElement } from "react";
 import TtrpgIntroComponent from "./components/intro/TtrpgIntroComponent";
-import ttrpgData from "../data/json/ttrpg.json";
 
 export default function TtrpgForm() {
-    const [loreHtml, setLoreHtml] = useState<ReactElement[]>([]);
-    const [displayedLore, setDisplayedLore] = useState<boolean[]>([]);
-
-    useEffect(() => {
-        let data: Array<boolean> = [];
-
-        for (let i = 0; i < ttrpgData.lore.data.length; i++)
-        {
-            data.push(false);
-        }
-
-        setDisplayedLore(data);
-    }, []);
-
-    useEffect(() => {
-        let data: Array<ReactElement> = [];
-
-        for (let i = 0; i < ttrpgData.lore.data.length; i++)
-        {
-            data.push(<div className="box is-flex flex-center-hor flex-center-ver ttrpg-lore" key={ttrpgData.lore.data[i].title} onClick={() => {
-                displayedLore[i] = !displayedLore[i];
-                setDisplayedLore([...displayedLore]);
-            }}>
-                {
-                    displayedLore[i]
-                    ? ttrpgData.lore.data[i].content.join("\n")
-                    : ttrpgData.lore.data[i].title
-                }
-            </div>)
-        }
-
-        setLoreHtml(data);
-    }, [ displayedLore ]);
+    const [tabIndex, setTabIndex] = useState<number>(0)
 
     return <>
         <QuoteComponent/>
@@ -45,7 +12,8 @@ export default function TtrpgForm() {
         <NavigationComponent />
         <div className="container box is-flex" id="ttrpg">
             <p className="mark">TTRPG</p>
-            { loreHtml }
+
+            <embed src="/files/ttrpg/character_sheet.pdf" width="500" height="375" type="application/pdf"></embed>
         </div>
     </>
 }
