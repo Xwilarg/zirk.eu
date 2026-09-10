1ad8b04e6b6855e81386f3fdda7f60897670d139
Xwilarg <xwilarg@protonmail.com>
2026-09-10 21:56:39 +0200

    Add Fainir

--- src/forms/OCForm.tsx
+++ src/forms/OCForm.tsx
@@ -6,13 +6,14 @@ import jsonSanara from "../../data/json/ocs/sanara.json"
 import jsonZirk from "../../data/json/ocs/zirk.json"
 import jsonYuzu from "../../data/json/ocs/yuzu.json"
 import jsonFish from "../../data/json/ocs/fish.json"
+import jsonFainir from "../../data/json/ocs/fainir.json"
 import OCBox from "../boxes/impl/OCBox";
 import ImageGroupModalForm, { type ImagePreviewInfo } from "../components/modal/ImageGroupModalForm";
 
 export default function OCform() {
     const [preview, setPreview] = useState<ImagePreviewInfo[] | null>(null);
     const characters = [
-        jsonSanara, jsonZirk, jsonYuzu, jsonFish
+        jsonSanara, jsonZirk, jsonYuzu, jsonFish, jsonFainir
     ]
 
     return <>
